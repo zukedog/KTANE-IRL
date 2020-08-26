7d99c6bfb6b4750480bdd412834cffcb88a9cfce
Zukedog <zukedog18@gmail.com>
2020-08-26 14:15:03 +0930

    add files for controller library

---
+++ libraries/KTANE_Controller/KTANE_Controller_Communication.h
@@ -0,0 +1,17 @@
+/*
+  KTANE_Controller_Communication.h - Library for storing module data on the controller module\
+  for JustinCase and Zukedog2000's Keep Talking and Nobody Explodes project.
+  Created by Zukedog2000, June 2020.
+*/
+#ifndef KTANE_Controller_Communication_h
+#define KTANE_Controller_Communication_h
+
+#include "Arduino.h"
+#include <Wire.h>
+
+class KTANE_Controller_Communication {
+  private:
+  public:
+};
+
+#endif
--- libraries/KTANE_Controller/KTANE_Controller_Module.h
+++ libraries/KTANE_Controller/KTANE_Controller_Module.h
@@ -1,15 +1,14 @@
 /*
-  KTANE_Controller.h - Library for storing module data on the controller module\
+  KTANE_Controller_Module.h - Library for storing module data on the controller module\
   for JustinCase and Zukedog2000's Keep Talking and Nobody Explodes project.
   Created by Zukedog2000, June 2020.
 */
-#ifndef KTANE_Controller_h
-#define KTANE_Controller_h
+#ifndef KTANE_Controller_Module_h
+#define KTANE_Controller_Module_h
 
 #include "Arduino.h"
 #include "KTANE_Controller_Communication.h"
-#include "KTANE_Controller_Module.h"
-class KTANE_Controller {
+class KTANE_Controller_Module {
   private:
   public:
 };
