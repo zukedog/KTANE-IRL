7f34dc4b644dff6d9d157d711d568219c8802808
Zukedog <zukedog18@gmail.com>
2020-09-16 09:05:23 +0930

    modify linux simlinks

--- tools/linux_symlinks.sh
+++ tools/linux_symlinks.sh
@@ -1,2 +1,1 @@
-ln -s ../libraries/KTANE_Controller_Lib ~/Arduino/libraries/KTANE_Controller_Lib
-ln -s ../libraries/KTANE_Module_Lib ~/Arduino/libraries/KTANE_Module_Lib
+ln -sR ../libraries/* ~/Arduino/libraries/
